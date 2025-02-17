bd9fe9d7836b8c7548939d89fb37595f3cb0034b
heixiaopengyou <163804893+heixiaopengyou@users.noreply.github.com>
2025-02-17 16:07:40 +0100

    Update Error_vector_from_PI_and_Ref_data.m

--- CURRENT-CONTROL-UNIT-SETUP/PREPROCESSING/Error_vector_from_PI_and_Ref_data.m
+++ CURRENT-CONTROL-UNIT-SETUP/PREPROCESSING/Error_vector_from_PI_and_Ref_data.m
@@ -8,7 +8,7 @@ ref_quad_volt = data(:, 8); % Quadrature voltage with Overshoot
 V_q = Ref_data_X(:,8); % Quadrature voltage without overshoot
 
 FirstCase_data = data(:, [4, 6, 8]); % Data (4) --> Reference Quadrature Current, Data (6) --> Measured Quadrature Curent, Data (8) --> PI Quadrature Voltage
-QuadVoltage_Error = V_q - FirstCase_data(:,3);
+QuadVoltage_Error = V_q - FirstCase_data(:,3); % Error vector used for training
 Full_dataset = [FirstCase_data, QuadVoltage_Error];
 columnNames =  {'Reference Quadrature Current', 'Measured Quadrature Curent', 'PI Quadrature Voltage', 'Error Vector'};
 Table_data = array2table(Full_dataset, 'VariableNames', columnNames);
